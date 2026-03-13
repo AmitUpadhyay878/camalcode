86285b921b2af01109c9e812746f16e155af53a3
Amitupadhyay878 <amitupadhyay878@gmail.com>
2026-03-13 22:34:37 +0530

    Review page and component with functionality-completed

---
+++ app/(protected)/reviews/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReviewClient from '@/modules/review/components/ReviewClient'
+
+
+const ReviewsPage = () => {
+  return (
+    <>
+    <ReviewClient />
+    </>
+  )
+}
+
+export default ReviewsPage
